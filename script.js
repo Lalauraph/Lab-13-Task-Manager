88dfda9436b87bcd0417204caf36b43ec40f5beb
Lalauraph <laurapalacio900@gmail.com>
2023-11-08 21:38:22 -0500

    script

---
+++ script.js
@@ -0,0 +1,91 @@
+import {
+  Task,
+  saveTask,
+  loadTasks
+} from './utils.js';
+
+const render = () => {
+
+
+  const todoSection = document.getElementById("todo");
+  const doingSection = document.getElementById("doing");
+  const doneSection = document.getElementById("done");
+  const form = document.getElementById("todo-form");
+  const taskInput = document.getElementById("taskInput");
+
+
+  function createCard(task) {
+    const card = task.createCard();
+
+    card.querySelector(".left-button").addEventListener("click", function () {
+      const previousSection = card.parentNode.previousElementSibling;
+      if (previousSection) {
+        previousSection.appendChild(card);
+        task.state = previousSection.id;
+        saveTasksToLocalStorage();
+
+      }
+    });
+
+    card.querySelector(".right-button").addEventListener("click", function () {
+      const nextSection = card.parentNode.nextElementSibling;
+      if (nextSection) {
+        nextSection.insertBefore(card, nextSection.firstChild);
+        task.state = nextSection.id;
+        saveTasksToLocalStorage();
+      }
+    });
+
+    card.querySelector(".delete-button").addEventListener("click", function () {
+      card.remove();
+      tasks = tasks.filter((t) => t !== task);
+      saveTasksToLocalStorage();
+    });
+
+    console.log(tasks.filter((t) => t !== task));
+    return card;
+  }
+
+  function saveTasksToLocalStorage() {
+    const tasks = Array.from(todoSection.children)
+      .concat(Array.from(doingSection.children))
+      .concat(Array.from(doneSection.children))
+      .map((card) => {
+        const text = card.querySelector("p").textContent;
+        const state = card.parentNode.id;
+        return new Task(text, state);
+      });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+
+  let tasks = loadTasks();
+  tasks.forEach((task) => {
+    let section;
+    if (task.state === "todo") {
+      section = todoSection;
+    } else if (task.state === "doing") {
+      section = doingSection;
+    } else if (task.state === "done") {
+      section = doneSection;
+    }
+
+    const card = createCard(task);
+    section.insertBefore(card, section.firstChild);
+    console.log(task.state);
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputText = taskInput.value;
+    const task = new Task(inputText);
+    const card = createCard(task);
+    todoSection.insertBefore(card, todoSection.firstChild);
+    saveTask(task);
+    taskInput.value = "";
+  });
+
+
+}
+window.onload = render;
